f41e8a7b43a12ba8bdfd13fedd9686eee16dd026
GomezRodrigo <gomezrodrigo93@hotmail.com>
2015-07-29 20:54:14 -0300

    Inicializacion de repositorio
    alicacion de consola

---
+++ consolaTestGibHub/consolaTestGibHub/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consolaTestGibHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola ADAM");
+            Console.ReadLine();
+        }
+    }
+}
